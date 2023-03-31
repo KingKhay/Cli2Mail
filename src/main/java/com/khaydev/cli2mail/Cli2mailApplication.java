82c2fad647f5ed65a88f21e30e05e92c2ec903a7
KingKhay <ebenezerbuabeng60@gmail.com>
2023-03-31 06:46:15 +0000

    Initial Commit

---
+++ src/main/java/com/khaydev/cli2mail/Cli2mailApplication.java
@@ -0,0 +1,13 @@
+package com.khaydev.cli2mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cli2mailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cli2mailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khaydev/cli2mail/Cli2mailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khaydev.cli2mail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cli2mailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
