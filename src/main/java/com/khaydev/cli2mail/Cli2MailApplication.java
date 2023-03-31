40ffbb7d4c7faa62cc6331a8bfa041f8edb65b9b
KingKhay <ebenezerbuabeng60@gmail.com>
2023-03-31 07:27:38 +0000

    set the exit code

--- src/main/java/com/khaydev/cli2mail/Cli2MailApplication.java
+++ src/main/java/com/khaydev/cli2mail/Cli2MailApplication.java
@@ -1,13 +1,35 @@
 package com.khaydev.cli2mail;
 
+import com.khaydev.cli2mail.command.MailCommand;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import picocli.CommandLine;
 
 @SpringBootApplication
-public class Cli2mailApplication {
+@RequiredArgsConstructor
+public class Cli2MailApplication implements CommandLineRunner, ExitCodeGenerator {
+
+	private final MailCommand mailCommand;
+	private final CommandLine.IFactory factory;
+
+	private int exitCode;
 
 	public static void main(String[] args) {
-		SpringApplication.run(Cli2mailApplication.class, args);
+		SpringApplication.run(Cli2MailApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		exitCode = new CommandLine(mailCommand, factory).execute(args);
+		System.exit(exitCode);
+	}
+
+
+	@Override
+	public int getExitCode() {
+		return exitCode;
+	}
 }
