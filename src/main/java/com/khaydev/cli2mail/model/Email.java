5600c7fa813127adc73abc2e6e9f0447690dbd96
KingKhay <ebenezerbuabeng60@gmail.com>
2023-03-31 07:10:26 +0000

    created a model for the email object

---
+++ src/main/java/com/khaydev/cli2mail/model/Email.java
@@ -0,0 +1,18 @@
+package com.khaydev.cli2mail.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Email {
+
+    private String from;
+    private String to;
+    private String subject;
+    private String message;
+}
