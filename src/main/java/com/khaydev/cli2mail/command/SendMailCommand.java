4b6bc7bafaedb7f94221294fd6e89a0d8da9e43e
KingKhay <ebenezerbuabeng60@gmail.com>
2023-03-31 08:25:43 +0000

    set the options list heading for the send mail command

--- src/main/java/com/khaydev/cli2mail/command/SendMailCommand.java
+++ src/main/java/com/khaydev/cli2mail/command/SendMailCommand.java
@@ -2,20 +2,19 @@ package com.khaydev.cli2mail.command;
 
 import com.khaydev.cli2mail.model.Email;
 import com.khaydev.cli2mail.service.EmailServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import picocli.CommandLine;
 
 @Component
-@CommandLine.Command(name = "send", description = "Command to send mail", mixinStandardHelpOptions = true)
+@CommandLine.Command(name = "send",
+        description = "Command to send mail",
+        mixinStandardHelpOptions = true,
+        optionListHeading = "%Options are")
+@RequiredArgsConstructor
 public class SendMailCommand implements Runnable{
-    private EmailServiceImpl service;
-
-    @Autowired
-    public SendMailCommand(EmailServiceImpl service) {
-        this.service = service;
-    }
+    private final EmailServiceImpl emailService;
 
     @Value("${mail.user.from}")
     String from;
@@ -38,7 +37,6 @@ public class SendMailCommand implements Runnable{
                 .subject(subject)
                 .message(message)
                 .build();
-
-        service.sendSimpleMail(email);
+        emailService.sendSimpleMail(email);
     }
 }
