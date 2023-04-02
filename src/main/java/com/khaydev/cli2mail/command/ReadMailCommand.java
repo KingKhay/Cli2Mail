0f42798cbcb74e5d949cb7b5a89b5d2a85ae4ae5
KingKhay <ebenezerbuabeng60@gmail.com>
2023-04-02 21:43:09 +0000

    added read mail command

---
+++ src/main/java/com/khaydev/cli2mail/command/ReadMailCommand.java
@@ -0,0 +1,9 @@
+package com.khaydev.cli2mail.command;
+
+import org.springframework.stereotype.Component;
+import picocli.CommandLine;
+
+@CommandLine.Command
+@Component
+public class ReadMailCommand {
+}
