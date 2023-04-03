fb13f1f01f81cd141f7ca26de3d64472189bfb6d
KingKhay <ebenezerbuabeng60@gmail.com>
2023-04-03 08:10:09 +0000

    removed read mail command

--- src/main/java/com/khaydev/cli2mail/command/ReadMailCommand.java
+++
@@ -1,9 +0,0 @@
-package com.khaydev.cli2mail.command;
-
-import org.springframework.stereotype.Component;
-import picocli.CommandLine;
-
-@CommandLine.Command
-@Component
-public class ReadMailCommand {
-}
